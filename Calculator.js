f11a5d8a9ff14d69abe7e9e9ab259479a945987f
ADEIKA, Blessing Isoyiza <blessadeik@gmail.com>
2022-04-30 20:47:29 +0100

    First Commit for Calculator

---
+++ Calculator.js
@@ -0,0 +1,28 @@
+// JavaScript for Calculator
+
+// add
+function add(number){
+    return number + number;
+}
+console.log(add(value));
+
+
+// multiply
+function multiply(number){
+    return number * value;
+}
+console.log(multiply(number));
+
+// subtract
+function subtract(number){
+    return number - number;
+}
+console.log(subtract(value));
+
+// divide
+function divide(number){
+    return number / value;
+}
+console.log(divide(value));
+
+// 
